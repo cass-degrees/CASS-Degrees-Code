1a22acfebb6868c7045cae855b2b602e1c4e5ed6
Daniel-Jang <37033052+Daniel-Jang@users.noreply.github.com>
2019-04-11 11:20:27 +1000

    Add, edit and delete records from the sample model using a custom-made very basic UI Form (#48)
    * Successfully made a very basic form that adds new records to the sampleModel. Next steps are to handle modification (PUT) and deletion (DELETE) of records.
    
    * Removed unused imports.
    
    * Successfully created more forms and code in ui/views.py to modify and delete records.
    
    * Incorporated James' base template to the sampleform.html for format consistency.
    
    * Followed ANU's Form styling guides as suggested by Jack.
    
    * Made changes suggested by James (Buttons aligned on the right and an empty line at the end of sampleform.html)
    
    * Clarified where the inspiration came from for the transfer of data from UI to API.

--- cassdegrees/ui/urls.py
+++ cassdegrees/ui/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, sampleform
 
 urlpatterns = [
     path('', index),
+    path('sampleform/', sampleform),
 ]
