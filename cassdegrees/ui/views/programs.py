fc2c3e7702ff7134ca31eedd501d3a351b9eaf61
Daniel-Jang <37033052+Daniel-Jang@users.noreply.github.com>
2019-05-01 09:41:50 +1000

    Template Duplication (#87)
    * Allow the user to click on "Create From" button in list.html, which opens the create program template with pre-filled form with data corresponding to the ID the user chose to duplicate from.
    
    * Altered the name of feature "Create from" to "Duplicate" as it is a better choice of phrase.
    
    * Corrected some error I have forgotten to change.
    
    * Made duplication variable assignment more consistent and set 'True' to 'true' in list.html for the duplication link, as lower case seems to be the web syntax.
    
    * Since CoursesInSubplanModel no longer exists due to Aiden's pull request, I simply removed it from the import list.
    Everything seems to work fine anyways so I will leave it like this (otherwise it crashes).
    
    * Removed redundant elif in list.html and fixed issues caused by the Degree name change to Programs

--- cassdegrees/ui/views/programs.py
+++ cassdegrees/ui/views/programs.py
@@ -6,6 +6,24 @@ from ui.forms import EditProgramFormSnippet
 
 
 def create_program(request):
+    duplicate = request.GET.get('duplicate', 'false')
+    if duplicate == 'true':
+        duplicate = True
+    elif duplicate == 'false':
+        duplicate = False
+
+    # Initialise instance with an empty string so that we don't get a "may be referenced before assignment" error below
+    instance = ""
+
+    # If we are creating a program from a duplicate, we retrieve the instance with the given id
+    # (should always come along with 'duplicate' variable) and return that data to the user.
+    if duplicate:
+        id = request.GET.get('id')
+        if not id:
+            return HttpResponseNotFound("Specified ID not found")
+        # Find the program to specifically create from:
+        instance = ProgramModel.objects.get(id=int(id))
+
     if request.method == 'POST':
         form = EditProgramFormSnippet(request.POST)
 
@@ -14,7 +32,10 @@ def create_program(request):
             return redirect('/list/?view=Program&msg=Successfully Added Program!')
 
     else:
-        form = EditProgramFormSnippet()
+        if duplicate:
+            form = EditProgramFormSnippet(instance=instance)
+        else:
+            form = EditProgramFormSnippet()
 
     return render(request, 'createprogram.html', context={
         "edit": False,
