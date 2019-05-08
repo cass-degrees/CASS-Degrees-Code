7f57ea53b85f091551f64ca0505d348b0dc40dce
Spencer <spencer.j411@gmail.com>
2019-05-08 17:46:23 +1000

    Bug Course Deletion Check v3000 (#103)
    * Added check for when selected courses are being used in any of the subplans before deletion.
    
    * reimplemented subplan check when deleting a course (blocks delete instead of giving a warning)
    
    * check now searches through program rules for subplans
    
    * rewrote subplan detection check and styled error message so it is easier on the eyes
    
    * rewrote subplan check; uses search api closer to its full potential. Code also checks programs (subplans and standalone courses)
    
    * added docstrings for inner functions in delete_course() function
    
    * Suggested changes for PR #103. (#109)
    
    * Suggested changes for PR #103.
    
    This will simplify the code and delete courses based on course codes rather than IDs in order to match the recent changes in PR #102
    
    * added comments

--- cassdegrees/ui/views/courses.py
+++ cassdegrees/ui/views/courses.py
@@ -1,8 +1,11 @@
-from api.models import CourseModel
-from django.http import HttpResponseNotFound
+from api.models import CourseModel, SubplanModel, ProgramModel
+from api.views import search
+
+from django.http import HttpResponseNotFound, HttpRequest
 from django.shortcuts import render, redirect
 
 from ui.forms import EditCourseFormSnippet
+import json
 
 
 def create_course(request):
@@ -27,18 +30,47 @@ def delete_course(request):
     data = request.POST
     instances = []
 
-    # TODO: Check if breaking subplans
-    # for course in courses_in_subplans:
-    #     # if course being deleted is in current subplan
-    #     if int(id_to_delete) == int(course['courseId_id']):
-    #         # TODO: improve this using django queries
-    #         used_subplans.extend(
-    #             [subplan['name'] + '(' + subplan['code'] + ')' for subplan in subplans if
-    #              course['subplanId_id'] == subplan['id']])
+    # Generate an internal request to search api made by Jack
+    gen_request = HttpRequest()
 
-    ids_to_delete = data.getlist('id')
-    for id_to_delete in ids_to_delete:
-        instances.append(CourseModel.objects.get(id=int(id_to_delete)))
+    # Grab all the courses in the database
+    gen_request.GET = {'select': 'id,code,year', 'from': 'course'}
+    courses = json.loads(search(gen_request).content.decode())
+
+    # ids of all the courses that were selected to be deleted
+    ids_to_delete = [int(course_id) for course_id in data.getlist('id')]
+    courses_to_delete = [c for c in courses if c['id'] in ids_to_delete]
+
+    error_msg = ""
+    instances = []
+
+    for course in courses_to_delete:
+        gen_request.GET = {'select': 'code', 'from': 'course', 'code': course['code']}
+        duplicate_courses = json.loads(search(gen_request).content.decode())
+        if len(duplicate_courses) < 2:
+            gen_request.GET = {'select': 'code,year,rules', 'from': 'subplan', 'rules': course['code']}
+            # subplans which depend on course where its code is equal to course['code']
+            subplans = json.loads(search(gen_request).content.decode())
+            gen_request.GET = {'select': 'code,year,rules', 'from': 'program', 'rules': course['code']}
+            # programs which depend on course where its code is equal to course['code']
+            programs = json.loads(search(gen_request).content.decode())
+
+            # if there are any subplans/programs that could be affected by the deletion of the selected courses
+            if len(subplans) > 0 or len(programs) > 0:
+                # compose error message
+                if len(subplans) > 0:
+                    error_msg += course['code'] + " is used in " + \
+                                 ", ".join(["{} ({})".format(sp['code'], sp['year']) for sp in subplans]) + \
+                                 ", and therefore cannot be deleted.\n"
+                if len(programs) > 0:
+                    error_msg += course['code'] + " is used in " + \
+                                 ", ".join(["{} ({})".format(sp['code'], sp['year']) for sp in programs]) + \
+                                 ", and therefore cannot be deleted.\n"
+                continue  # dont append course to the list instances
+        instances.append(CourseModel.objects.get(id=course['id']))
+
+    if len(error_msg) > 0:
+        return redirect('/list/?view=Course&error=Failed to Delete Course(s)!\n' + error_msg)
 
     if "confirm" in data:
         for instance in instances:
