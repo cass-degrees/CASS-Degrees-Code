fb597302adde92a499e98609b185cecadcca601f
Josh Bee <joshbee89@gmail.com>
2019-05-26 20:24:34 +1000

    Insert path to bulk upload functionality. (#162)

--- cassdegrees/ui/views/index.py
+++ cassdegrees/ui/views/index.py
@@ -12,7 +12,8 @@ def index(request):
         {'url': "/create/program/", 'kind': "fas fa-file-medical", 'label': "Create Program Template"},
         {'url': "/create/subplan/", 'kind': "fas fa-clipboard-list", 'label': "Create Subplan"},
         {'url': "/create/course/", 'kind': "fas fa-th", 'label': "Create Course"},
-        {'url': "/list/", 'kind': "fas fa-edit", 'label': "Manage Existing Programs, Subplans & Courses"}
+        {'url': "/list/", 'kind': "fas fa-edit", 'label': "Manage Existing Programs, Subplans & Courses"},
+        {'url': "/bulk_upload/", 'kind': "fas fa-sign-in-alt", 'label': "Bulk Upload"}
     ]
 
     # Dynamically calculate expected width for buttons
