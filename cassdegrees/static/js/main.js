8e2dc20fc0b17bfb69afd53e572140ca63ca6976
James <u6379426@anu.edu.au>
2019-09-12 09:06:56 +0000

    Implement custom checkboxes globally (#336)
    * Implement custom checkboxes globally
    
    * Fix issues with Chrome/Edge

--- cassdegrees/static/js/main.js
+++ cassdegrees/static/js/main.js
@@ -28,3 +28,55 @@ function goBack() {
 function returnToList(pageName) {
     window.location.href = STAFF_URL_PREFIX + "list/?view="+pageName;
 }
+
+// Attempt to replace all checkboxes on the page with modern equivalents, noting that browsers don't typically
+// allow for custom checkbox styling, we have to implement this logic ourselves.
+// This preserves all element semantics, including JavaScript event handlers and so forth.
+function updateCheckboxes() {
+    document.querySelectorAll("input[type=checkbox]").forEach(function(element) {
+        if (element.classList.contains("custom-checkbox")) {
+            return;
+        }
+
+        element.classList.add("custom-checkbox");
+
+        const parentElement = element.parentElement;
+
+        // Create a new subelement to move this element to
+        const newChild = document.createElement("div");
+        newChild.classList.add("pretty", "p-icon");
+        newChild.style = "font-size: 25px";
+
+        // Create a sibling element with a label which can be mutated by CSS
+        const newSibling = document.createElement("div");
+        newSibling.classList.add("state", "p-primary");
+
+        // Add a font awesome icon
+        const fontAwesomeIcon = document.createElement("i");
+        fontAwesomeIcon.classList.add("icon", "fa", "fa-check");
+
+        // Create the label, which the CSS library injects onto
+        const newLabel = document.createElement("label");
+        newLabel.innerHTML = "&nbsp;";
+        newSibling.appendChild(fontAwesomeIcon);
+        newSibling.appendChild(newLabel);
+
+        // Composite our new subelement together
+        newChild.appendChild(parentElement.removeChild(element));
+        newChild.appendChild(newSibling);
+
+        // By now, we have the following:
+        // <div class="pretty p-default">
+        //    <input type="checkbox" (any other attributes on the original checkbox, including name/id/events)>
+        //    <div class="state p-primary">
+        //      <i class="icon fa fa-check"></i>
+        //      <label>&nbsp;</label>
+        //    </div>
+        // </div>
+
+        // Insert this new big element into our original parent, as if nothing ever happened
+        parentElement.appendChild(newChild);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", updateCheckboxes);
