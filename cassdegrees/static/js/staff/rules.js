22f8a29573732d188259d3045450809dca9e7d98
Daniel-Jang <37033052+Daniel-Jang@users.noreply.github.com>
2019-09-17 22:25:19 +1000

    Feature Duplicate Rules (#340)
    * Added additional functions to duplicate rules. Made it so that when the user presses 'duplicate', the copy is created right underneath the source rule.
    
    * Removed unnecessary line
    
    * Made duplication work for course creation/edit.

--- cassdegrees/static/js/staff/rules.js
+++ cassdegrees/static/js/staff/rules.js
@@ -891,9 +891,19 @@ Vue.component('rule_either_or', {
             this.count_units();
             this.do_redraw();
         },
+        duplicate_rule: function(index, group) {
+            // JSON.parse(JSON.stringify(...)) is done to actually duplicate the contents of the rule, rather than just copying the memory references.
+            this.details.either_or[group].splice(index, 0, JSON.parse(JSON.stringify(this.details.either_or[group][index])));
+            this.do_redraw();
+        },
         remove_group: function(group) {
             this.details.either_or.splice(group, 1);
             this.count_units();
+            this.do_redraw();
+        },
+        duplicate_group: function(group) {
+            // JSON.parse(JSON.stringify(...)) is done to actually duplicate the contents of the rule, rather than just copying the memory references.
+            this.details.either_or.splice(group, 0, JSON.parse(JSON.stringify(this.details.either_or[group])));
             this.do_redraw();
         },
         check_options: function() {
@@ -1013,6 +1023,11 @@ Vue.component('rule_container', {
         },
         remove: function(index) {
             this.rules.splice(index, 1);
+            this.do_redraw();
+        },
+        duplicate_rule: function(index) {
+            // JSON.parse(JSON.stringify(...)) is done to actually duplicate the contents of the rule, rather than just copying the memory references.
+            this.rules.splice(index, 0, JSON.parse(JSON.stringify(this.rules[index])));
             this.do_redraw();
         },
         check_options: function() {
