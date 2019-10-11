b7ed24722db12b9524f38a398b6431b5bbb2c1cc
James <u6379426@anu.edu.au>
2019-10-11 10:05:06 +1000

    Truncate courses in course lists in PDFs to avoid filling up entire page (#456)
    * Truncate courses in course lists in PDFs to avoid filling up entire page
    
    * Implement the rendering of course lists on another page
    
    * Remove debug message
    
    * Shink message for appendix reference
    
    * Fix PIP

--- cassdegrees/ui/templatetags/course_boxes.py
+++ cassdegrees/ui/templatetags/course_boxes.py
@@ -51,6 +51,56 @@ def course_box_with_values(context, count, courses, plan):
 
     return Template(output).render(context)
 
+
+@register.simple_tag(takes_context=True)
+def truncate_course_list(context, courses, cache_large_lists, count=5):
+    if len(courses) > count and cache_large_lists:
+        # Store this for later, and assign it an appendix id
+        if not hasattr(context, "large_course_lists"):
+            context.large_course_lists = {}
+
+        id = len(context.large_course_lists) + 1  # Zero-indexed -> human readable
+
+        context.large_course_lists[id] = courses
+
+        return Template("See appendix #" + str(id) + ".").render(context)
+
+    output = ""
+    for i in range(0, len(courses)):
+        output += str(courses[i]['code'])
+
+        if i + 1 < len(courses):
+            output += ", "
+
+    return Template(output).render(context)
+
+
+@register.simple_tag(takes_context=True)
+def print_extended_course_lists(context):
+    if not hasattr(context, "large_course_lists"):
+        return Template("").render(context)
+
+    output = "<div class=\"break-page\"></div>" \
+             "<div class=\"columns-4 columns-no-gap\">" \
+             "<h2>Appendix</h2>"
+
+    for (id, courses) in context.large_course_lists.items():
+        output += "<h3>Course List #" + str(id) + ":</h3>"
+
+        output += "<div>"
+
+        for i in range(0, len(courses)):
+            output += str(courses[i]['code'])
+
+            if i + 1 < len(courses):
+                output += ", "
+
+        output += "</div>"
+
+    output += "</div>"
+
+    return Template(output).render(context)
+
 # https://stackoverflow.com/questions/4651172/reference-list-item-by-index-within-django-template/29664945#29664945
 @register.filter
 def index(List, i):
